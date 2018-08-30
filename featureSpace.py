ab216aeff25b2ce4f8ad7bb43668a9e68f2460b3
Chong Sun <sonshicyu@gmail.com>
2018-08-30 09:54:27 -0500

    Group the code of getting data in to a function

--- featureSpace.py
+++ featureSpace.py
@@ -1,17 +1,23 @@
 import numpy as np
 
 def featureTransform(rawData):
-    x1, x2 = rawData[0], rawData[1]
-    feature = [1, x1, x2, x1**2, x2**2, x1*x2, abs(x1-x2), abs(x1+x2)]
+    x1, x2, y = rawData[0], rawData[1], rawData[2]
+    feature = [1, x1, x2, x1**2, x2**2, x1*x2, abs(x1-x2), abs(x1+x2), y]
     return feature
 
 def getFeature(rawInput) :
     feature = map(featureTransform, rawInput)
     return feature 
 
-dataFile = open('in.dta')
-rawData = [map(eval, line.strip(" ").rstrip("\r\n").split("  ")) for line in dataFile]
-feature = np.array(getFeature(rawData))
+def transformData(fileName):
+    dataFile = open(fileName)
+    rawData = [map(eval, line.strip(" ").rstrip("\r\n").split("  ")) for line in dataFile]
+    feature = np.array(getFeature(rawData))
+    return feature
+    
+
+dataFile = "in.dta"
+feature = transformData(dataFile)
 
 
 
